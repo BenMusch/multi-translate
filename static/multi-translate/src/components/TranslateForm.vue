821ae1ea015adde0475996882b21aa345dab18c5
Ben Muschol <benmuschol@gmail.com>
2018-03-20 23:27:08 -0400

    Get form working with a no-op submit function

--- static/multi-translate/src/components/TranslateForm.vue
+++ static/multi-translate/src/components/TranslateForm.vue
@@ -1,11 +1,12 @@
 <template>
   <v-container fluid>
     <h1>Create your translation</h1>
-    <v-form>
+    <v-form @submit.prevent="submit">
       <v-text-field
         name="text"
         label="Text to translate"
         placeholder="Hello, world"
+        v-model="text"
         multi-line
         rows="1"
       ></v-text-field>
@@ -14,27 +15,30 @@
         label="Number of languages to translate through"
         type="number"
         placeholder="5"
+        v-model="languageCount"
         :full-width="false"
       ></v-text-field>
-      <v-btn>Translate</v-btn>
+      <v-btn @click.prevent="submit">Translate</v-btn>
     </v-form>
   </v-container>
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'TranslateForm',
+  data() {
+    return {
+      text: "",
+      languageCount: ""
+    }
+  },
+
+  methods: {
+    submit() {
+      console.log(this.text, this.languageCount)
+    }
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+</script>
