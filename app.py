5862ddce80a400a6368260db653c20867b783a11
Ben Muschol <benmuschol@gmail.com>
2018-03-23 12:03:33 -0400

    Update CORS config

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import logging
+import os
 
-from chalice import Chalice
+from chalice import Chalice, CORSConfig
 
 from chalicelib import translate
-
 
 app = Chalice(app_name='multi-translate')
 app.log.setLevel(logging.DEBUG)
 
-@app.route("/")
+cors_config = CORSConfig(allow_origin=os.environ['TRANSLATE_ORIGIN'])
+
+@app.route("/", cors=cors_config)
 def index():
     return translate.available_languages
 
-@app.route("/translate", methods=["POST"], cors=app.debug)
+@app.route("/translate", methods=["POST"], cors=cors_config)
 def perform_translation():
     request_data = app.current_request.json_body
     result = translate.translate(request_data["text"], int(request_data["numTranslations"]))
