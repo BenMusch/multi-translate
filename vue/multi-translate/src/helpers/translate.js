a9b4e76d265fa62bcb783f607603659b565beffc
Ben Muschol <benmuschol@gmail.com>
2018-03-25 14:51:55 -0400

    Reset url

--- vue/multi-translate/src/helpers/translate.js
+++ vue/multi-translate/src/helpers/translate.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const TRANSLATE_URL = 'http://localhost:8000/translate'
+const TRANSLATE_URL = 'https://266ic11wl4.execute-api.us-east-1.amazonaws.com/api/translate'
 
 const translatePhrase = (text, numTranslations) => {
   return axios({
